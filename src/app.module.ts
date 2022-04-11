33f829d9d4dbaec05dbd390a680b00d07cbad50d
Kennan Fattahillah <kennfatt@gmail.com>
2022-04-11 21:54:01 +0700

    feat: implement PrismaService into the app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KeluargaModule } from './keluarga/keluarga.module';
+import { PrismaService } from './services/prisma.service';
 
 @Module({
   imports: [KeluargaModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
