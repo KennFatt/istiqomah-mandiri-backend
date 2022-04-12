f05a5b6887bafabd4611c6494667cc42158d2b25
Kennan Fattahillah <kennfatt@gmail.com>
2022-04-12 22:27:59 +0700

    feat: add working DTO for creating new keluarga
    - chore: add `class-transformer` and `class-validator`

--- src/keluarga/dto/create-keluarga.dto.ts
+++ src/keluarga/dto/create-keluarga.dto.ts
@@ -1,1 +1,55 @@
-export class CreateKeluargaDto {}
+import {
+  IsBoolean,
+  IsNumber,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  Length,
+  ValidationArguments,
+} from 'class-validator';
+
+export class CreateKeluargaDto {
+  @IsString()
+  @IsNumberString(null, { message: isNumberStringMessage() })
+  no_induk: string;
+
+  @IsOptional()
+  @IsNumberString(null, { message: isNumberStringMessage() })
+  @Length(16, 16, { message: lengthConstraintMessage(16) })
+  no_kk: string = '';
+
+  @IsOptional()
+  @IsNumberString(null, { message: isNumberStringMessage() })
+  @Length(16, 16, { message: lengthConstraintMessage(16) })
+  no_nik: string = '';
+
+  @IsOptional()
+  @IsString()
+  address: string = '';
+
+  @IsOptional()
+  @IsBoolean()
+  is_registered: boolean = false;
+
+  @IsNumber()
+  kepala_keluarga_id: number;
+}
+
+/**
+ * Helper function to generate error message
+ *
+ * @private
+ */
+function isNumberStringMessage() {
+  return (arg: ValidationArguments) => `${arg.property} harus bernilai numeric`;
+}
+
+/**
+ * Helper function to generate error message
+ *
+ * @private
+ */
+function lengthConstraintMessage(length: number) {
+  return (arg: ValidationArguments) =>
+    `${arg.property} harus memiliki ${length} digit karakter`;
+}
